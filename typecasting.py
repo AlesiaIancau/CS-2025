08b652ea56d7e54e8b9713ac88ca7c9b3f2eaca2
AlesiaIancau <alesiaiancau23@gmail.com>
2025-09-24 12:24:51 +0800

    Variables 24.09

---
+++ typecasting.py
@@ -0,0 +1,17 @@
+# Typecasting = the process of converting a variable from one data type to another
+
+name = "Dan"
+age = 21
+gpa = 3.2
+is_student = True
+
+print(type(name))
+
+gpa = int(gpa)
+print(gpa)
+
+age = float(age)
+print(age)
+
+name = bool(name)
+print(name)
---
+++ variables.py
@@ -0,0 +1,22 @@
+#strings
+first_name = "Anna"
+food = "pizza"
+email = "anna19@gmail.com"
+print(f"My name is {first_name}")
+
+#integers
+age = 5
+quantity = 3
+
+#float
+price = 10.75
+gpa = 3.5
+
+#boolean
+is_student = False
+
+if is_student:
+    print("He is a student")
+else:
+    print("He is NOT a student")
+
